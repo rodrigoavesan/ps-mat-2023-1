2903bd284448f8c71b85ed2d9213b4a2fde6144a
Rodrigo Avelar <82687467+rodrigoavesan@users.noreply.github.com>
2023-04-18 13:58:01 +0000

    (18/04) Criação do Front-end

---
+++ front-end/src/pages/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Login(){
+    return(
+        <>
+        <h1>Página de Login</h1>
+        <p>Futura página de login</p> 
+        </>
+    )
+}
