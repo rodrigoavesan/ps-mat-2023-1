5b726ae839f5c137506ebf30d12abb2198f99185
Rodrigo Avelar <82687467+rodrigoavesan@users.noreply.github.com>
2023-06-01 13:42:40 +0000

    (01/06) Corrigidas configurações de conexão ao BD do back-end

--- back-end/config/config.js
+++ back-end/config/config.js
@@ -23,11 +23,11 @@ module.exports = {
     }*/
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "dialect": process.env.DB_DIALECT,
     /*"define": {
       "underscored": true,
       "underscoredAll": true
