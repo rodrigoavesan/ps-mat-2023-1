52274b1cb9321da69e8733d56c4baff145b2fe4b
Rodrigo Avelar <82687467+rodrigoavesan@users.noreply.github.com>
2023-06-06 13:34:53 +0000

    (06/05) Corrigindo o Cors

--- back-end/app.js
+++ back-end/app.js
@@ -1,39 +1,35 @@
-//Carrega as variaveis de ambiente do arquivo .env para a aplicação
+// Carrega as variáveis de ambiente do arquivo
+// .env para a aplicação
 require('dotenv').config()
 
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
+// Habilita que apenas o front-end indicado
+// na variável process.env.FRONT_ORIGIN possa
+// acessar o back-end
 const cors = require('cors')
 app.use(cors({
   origin: process.env.FRONT_ORIGIN,
-  credentials: true //Exige o envio de cookie com credenciais
+  credentials: true // Exige o envio de cookie com credenciais
 }))
 
-
-// Conexão ao BD ---------------------------------------------------------------------
+// Conexão ao BD ------------------------------------------
 const db = require('./models')
 
 try {
   db.sequelize.authenticate()
-  console.log('SEQUELIZE: connection has been established successfully')
+  console.log('SEQUELIZE: connection has been established successfully.')
 }
-catch(error){
-  console.log('* SEQUELIZE: unable to connect to the database', error)
-  process.exit(1) //Encerra o servidor com erro  
+catch(error) {
+  console.error('* SEQUELIZE: unable to connect to the database: ', error)
+  process.exit(1)     // Encerra o servidor com erro
 }
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// ---------------------------------------------------------
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -41,49 +37,36 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//Chama a verificação de autenticação para qualquer rota
+// Chama a verificação de autenticação para qualquer rota
 const auth = require('./lib/auth')
 app.use(auth)
 
-/**********************************Rotas******************************** */
+/********************* ROTAS ***************************/
 const users = require('./routes/users')
 app.use('/users', users)
-
 
 const channels = require('./routes/channels')
 app.use('/channels', channels)
 
+const cities = require('./routes/cities')
+app.use('/cities', cities)
+
+const customerTags = require('./routes/customer_tags')
+app.use('/customer_tags', customerTags)
+
+const customers = require('./routes/customers')
+app.use('/customers', customers)
+
 const paymentMethods = require('./routes/payment_methods')
 app.use('/payment_methods', paymentMethods)
-
-const carriers = require('./routes/carriers')
-app.use('/carriers', carriers)
-
-const shipmentPriorities = require('./routes/shipment_priorities')
-app.use('/shipment_priorities', shipmentPriorities)
-
-const orderStatuses = require('./routes/order_statuses')
-app.use('/order_statuses', orderStatuses)
 
 const tags = require('./routes/tags')
 app.use('/tags', tags)
 
-const orderTags = require('./routes/order_tags')
-app.use('/order_tags', orderTags)
+const suppliers = require('./routes/suppliers')
+app.use('/suppliers', suppliers)
 
-const customerTag = require('./routes/customer_tags')
-app.use('/customer_tags', customerTag)
-
-const orderRelStatuses = require('./routes/order_rel_statuses')
-app.use('/order_rel_statuses', orderRelStatuses)
-
-const order = require('./routes/orders')
-app.use('/orders', order)
-
-const cities = require('./routes/cities')
-app.use('/cities', cities)
-
-const customers = require('./routes/customers')
-app.use('/customers', customers)
+const products = require('./routes/products')
+app.use('/products', products)
 
 module.exports = app;
